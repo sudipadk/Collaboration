212ed3d08a2168f27ac75f6f7a8870596bc8956c
Sudip Adhikari <sudip.adhk1@gmail.com>
2023-12-01 14:47:49 +0545

    Added innerHTML for replacing the text

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const getColor= () =>{
-
+    
+    const h5=document.getElementById('head3');
     
     let colorchange =Math.random();
     const randomColor="#"+ colorchange.toString(16).slice(2,8);
-    // console.log(randomcolor);
+    console.log(randomColor);
+    
+    h5.innerHTML=randomColor;
     
     document.body.style.backgroundColor=randomColor ;
 }
 
-
-    changeColor.addEventListener('click', getColor)
+    changeColor.addEventListener('click', getColor);
