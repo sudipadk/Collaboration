1223d23dcedde57b1926757c3dd0e5fcfa3276b0
urekagautam <152254845+urekagautam@users.noreply.github.com>
2023-12-03 22:35:52 +0545

    Added Interval for start button(defined startFn)

--- task2ureka.js
+++ task2ureka.js
@@ -1,6 +1,7 @@
 const start = document.getElementById('start');
 const end = document.getElementById('end');
 
+let Interval;
 start.addEventListener('click',function(){
     startFn();
 });
@@ -8,3 +9,13 @@ start.addEventListener('click',function(){
 end.addEventListener('click',function(){
     endFn();
 });
+
+function startFn(){
+    Interval = setInterval(function(){
+        let colorchange = Math.random();
+        const randomColor = "#" + colorchange.toString(16).slice(2,8);
+        console.log(randomColor);
+
+        document.body.style.backgroundColor=randomColor ;
+    }, 1000);
+}
