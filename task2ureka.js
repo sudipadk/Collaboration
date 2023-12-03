53174478bb948ee70af7745cb1a18bf12d40e292
urekagautam <152254845+urekagautam@users.noreply.github.com>
2023-12-03 22:33:56 +0545

    Event Listener Added to start and end btn

---
+++ task2ureka.js
@@ -0,0 +1,10 @@
+const start = document.getElementById('start');
+const end = document.getElementById('end');
+
+start.addEventListener('click',function(){
+    startFn();
+});
+
+end.addEventListener('click',function(){
+    endFn();
+});
